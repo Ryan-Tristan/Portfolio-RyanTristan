b61004d52b327ef033990ae4e03ddf482cafc6ff
Ryan-Tristan <94442364+Ryan-Tristan@users.noreply.github.com>
2024-10-13 02:48:49 +0700

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react'
-import ThemeContext from '../context/themecontext'
+import ThemeContext from '../context/ThemeContext'
 const Contact = () => {
   const {DarkMode} = useContext(ThemeContext)
   return (
